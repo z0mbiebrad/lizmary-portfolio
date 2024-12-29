6879cbd4947844bccfd081dd7460ee15c4af465e
z0mbiebrad <bradleyt.evans@gmail.com>
2024-12-29 07:04:42 -0500

    finished up the "print publications" page

--- resources/views/livewire/print-publication.blade.php
+++ resources/views/livewire/print-publication.blade.php
@@ -1,9 +1,9 @@
-<div class="overflow-hidden bg-white py-24 sm:py-32">
+<div class="overflow-hidden bg-white py-16 sm:py-24 md:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div
-            class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            class="mx-auto grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:w-4/5">
             <div class="lg:pr-8 lg:pt-4">
-                <div class="lg:max-w-lg">
+                <div class="">
                     <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-[#e9ad85] sm:text-5xl">
                         Print Publications
                     </p>
@@ -16,44 +16,64 @@
                     </p>
                     <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
                         <div class="relative pl-9">
-                            <dd class="inline">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
-                                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                            <dt class="inline font-semibold text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="absolute left-1 top-1 size-5 text-indigo-600">
+                                    <path d="M168 80c-13.3 0-24 10.7-24 24l0 304c0 8.4-1.4 16.5-4.1 24L440 432c13.3 0 24-10.7 24-24l0-304c0-13.3-10.7-24-24-24L168 80zM72 480c-39.8 0-72-32.2-72-72L0 112C0 98.7 10.7 88 24 88s24 10.7 24 24l0 296c0 13.3 10.7 24 24 24s24-10.7 24-24l0-304c0-39.8 32.2-72 72-72l272 0c39.8 0 72 32.2 72 72l0 304c0 39.8-32.2 72-72 72L72 480zM176 136c0-13.3 10.7-24 24-24l96 0c13.3 0 24 10.7 24 24l0 80c0 13.3-10.7 24-24 24l-96 0c-13.3 0-24-10.7-24-24l0-80zm200-24l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/>
+                                </svg>
+                                Featured Project:
+                            </dt>
+                            <dd class="inline text-white hover:text-black">
+                                <a 
+                                    href="https://issuu.com/pinestrawmag/docs/bridal_guide_2024" 
+                                    target="_blank"
+                                    class="text-[#e9ad85] underline hover:no-underline focus:no-underline"
+                                >
+                                    2024 Bride & Groom by Pinestraw Magazine
+                                </a>
+                            </dd>
                         </div>
                         <div class="relative pl-9">
                             <dt class="inline font-semibold text-gray-900">
-                                <svg class="absolute left-1 top-1 size-5 text-indigo-600" viewBox="0 0 20 20"
-                                    fill="currentColor" aria-hidden="true" data-slot="icon">
-                                    <path fill-rule="evenodd"
-                                        d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z"
-                                        clip-rule="evenodd" />
+                                <svg class="absolute left-1 top-1 size-5 text-indigo-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path d="M368.4 18.3L312.7 74.1 437.9 199.3l55.7-55.7c21.9-21.9 21.9-57.3 0-79.2L447.6 18.3c-21.9-21.9-57.3-21.9-79.2 0zM288 94.6l-9.2 2.8L134.7 140.6c-19.9 6-35.7 21.2-42.3 41L3.8 445.8c-3.8 11.3-1 23.9 7.3 32.4L164.7 324.7c-3-6.3-4.7-13.3-4.7-20.7c0-26.5 21.5-48 48-48s48 21.5 48 48s-21.5 48-48 48c-7.4 0-14.4-1.7-20.7-4.7L33.7 500.9c8.6 8.3 21.1 11.2 32.4 7.3l264.3-88.6c19.7-6.6 35-22.4 41-42.3l43.2-144.1 2.7-9.2L288 94.6z"/>
                                 </svg>
-                                SSL certificates.
+                                Roles:
                             </dt>
-                            <dd class="inline">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-                                cupidatat commodo.</dd>
-                        </div>
-                        <div class="relative pl-9">
-                            <dt class="inline font-semibold text-gray-900">
-                                <svg class="absolute left-1 top-1 size-5 text-indigo-600" viewBox="0 0 20 20"
-                                    fill="currentColor" aria-hidden="true" data-slot="icon">
-                                    <path
-                                        d="M4.632 3.533A2 2 0 0 1 6.577 2h6.846a2 2 0 0 1 1.945 1.533l1.976 8.234A3.489 3.489 0 0 0 16 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234Z" />
-                                    <path fill-rule="evenodd"
-                                        d="M4 13a2 2 0 1 0 0 4h12a2 2 0 1 0 0-4H4Zm11.24 2a.75.75 0 0 1 .75-.75H16a.75.75 0 0 1 .75.75v.01a.75.75 0 0 1-.75.75h-.01a.75.75 0 0 1-.75-.75V15Zm-2.25-.75a.75.75 0 0 0-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 0 0 .75-.75V15a.75.75 0 0 0-.75-.75h-.01Z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                Database backups.
-                            </dt>
-                            <dd class="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna
-                                sit morbi lobortis.</dd>
+                            <dd class="inline">
+                                Writer, Designer
+                            </dd>
                         </div>
                     </dl>
                 </div>
             </div>
-            <img src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png"
-                alt="Product screenshot"
-                class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-                width="2432" height="1442">
+                <a href="https://www.thepilot.com/showstopper-jeki-yoo-brings-his-magic-to-bpac/article_7e71d7ac-b3e2-11ee-b47b-e757d9d5a135.html" target="_blank">
+                    <img 
+                        class="rounded-xl shadow-xl ring-1 ring-gray-400/10 transform transition duration-300 hover:scale-125"
+                        src="{{ asset('images/jekiyoo.png') }}"
+                        alt="Product screenshot"
+                        width="2432" 
+                        height="1442"
+                    >
+                </a>
+                <a href="https://www.thepilot.com/schoolhouse-rock-live-to-rock-bpac/article_bfaa715c-b70c-11ee-a496-6354b5794098.html" target="_blank">
+                    <img 
+                        class="rounded-xl shadow-xl ring-1 ring-gray-400/10 transform transition duration-300 hover:scale-125"
+                        src="{{ asset('images/schoolhouse.png') }}"
+                        alt="Product screenshot" 
+                        width="2432" 
+                        height="1442"
+                    >
+                </a>
+                <a href="https://www.thepilot.com/travel-lightly-sleep-quietly-with-an-oral-appliance/article_dd83c68e-a98c-11ee-aaec-df399688f663.html#comments" target="_blank">
+                    <img 
+                        class="rounded-xl shadow-xl ring-1 ring-gray-400/10 transform transition duration-300 hover:scale-125"
+                        src="{{ asset('images/dental.png') }}"
+                        alt="Product screenshot"
+                        width="2432" 
+                        height="1442"
+                    >
+                </a>
+            </div>
         </div>
     </div>
 </div>
